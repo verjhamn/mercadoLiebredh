48acef5ccfb27864a8ad135cc39ce4d41f120005
Geovany Rico <geo981202@gmail.com>
2023-01-17 18:58:35 +0100

    Proyecto inicializado

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const PORT = 3001
+
+app.use(express.static('src/public'))
+
+app.get('/', (_,res)=>{
+    res.sendFile(path.resolve('src/views/home.html'))
+})
+
+app.get('/home', (_,res)=>{
+    res.sendFile(path.resolve('src/views/home.html'))
+})
+
+app.get('/register', (_,res)=>{
+    res.sendFile(path.resolve('src/views/register.html'))
+})
+
+app.listen(PORT, ()=>{
+    console.log('Servidor corriendo en el puerto ', PORT)
+})
