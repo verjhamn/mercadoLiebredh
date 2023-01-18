539cf13bba45d470bb48732eb4afedee1bcdd2c6
Geovany Rico <geo981202@gmail.com>
2023-01-18 11:25:06 -0500

    Funcionalidades terminadas

---
+++ src/public/js/login.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    console.log("ready!")
+
+    $("#btn-ingresar").on("click", function (event) {
+        location.href ='/home'
+    })
+
+})
--- src/public/js/register.js
+++ src/public/js/register.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     console.log("ready!")
 
-    $("#btn-enviar").on("click", function (event) {
-        location.href ='/home';
-    })
-
     $("#btn-borrar").on("click", function (event) {
         $("#datosRegistro")[0].reset()
+    })
+
+    $("#btn-modal").on("click", function (event) {
+        location.href = '/home'
     })
 
 })
